e5c9b9b9902e95575ff338466661d5549a723b99
AbishekJUNGthakuri19 <abishekjung00@gmail.com>
2025-02-13 12:00:12 +0545

    navbar

---
+++ src/assets/Data.js
@@ -0,0 +1,65 @@
+import satinblackback from './images/satinblackback.webp';
+import satinblackfront from './images/satinblackfront.webp';
+import satinbluefront from './images/satinbluefront.webp';
+import satinblueback from './images/satinblueback.webp';
+import satinredfront from './images/satinredfront.webp';
+import satinredback from './images/satin_red_back.webp';
+import satingreyfront from './images/satingreyfront.webp';
+import satingreyback from './images/satingreyback.webp';
+import satingreenfront from './images/satingreenfront.webp';
+import satingreenback from './images/satingreenback.webp';
+import brocadenewhoodiefront from './images/brocadenewhoodiefront.webp';
+import brocadenewhoodieback from './images/brocadenewhoodieback.webp';
+
+export  const dropIV = [
+    {
+        id:1,
+        name : 'SATIN BLACK BOMBER JACKET',
+        front_img : satinblackfront,
+        back_img: satinblackback,
+        price: 5000.00,
+        desc : "Introducing the Satin Bomber Jacket—a bold and stylish addition to your wardrobe. Crafted with a sleek satin finish, it features striking double white stripes on the shoulders, a ribbed collar, cuffs, and hem for a snug fit, and the 'Brocade' logo embroidered on the sleeve. This jacket comes in various color options, making it perfect for elevating your streetwear look with a touch of luxury and modern flair."
+    },
+    {
+        id:2,
+        name:"SATIN BLUE BOMBER JACKET",
+        front_img :satinbluefront ,
+        back_img : satinblueback,
+        price: 5000.00,
+        desc: "Introducing the Satin Bomber Jacket—a bold and stylish addition to your wardrobe. Crafted with a sleek satin finish, it features striking double white stripes on the shoulders, a ribbed collar, cuffs, and hem for a snug fit, and the 'Brocade' logo embroidered on the sleeve. This jacket comes in various color options, making it perfect for elevating your streetwear look with a touch of luxury and modern flair."
+    },
+    {
+        id:3,
+        name:"SATIN RED BOMBER JACKET",
+        front_img : satinredfront,
+        back_img : satinredback,
+        price: 5000.00,
+        desc: "Introducing the Satin Bomber Jacket—a bold and stylish addition to your wardrobe. Crafted with a sleek satin finish, it features striking double white stripes on the shoulders, a ribbed collar, cuffs, and hem for a snug fit, and the 'Brocade' logo embroidered on the sleeve. This jacket comes in various color options, making it perfect for elevating your streetwear look with a touch of luxury and modern flair."
+    },
+    {
+        id:4,
+        name:"SATIN GREY BOMBER JACKET",
+        front_img :satingreyfront ,
+        back_img : satingreyback,
+        price: 5000.00,
+        desc: "Introducing the Satin Bomber Jacket—a bold and stylish addition to your wardrobe. Crafted with a sleek satin finish, it features striking double white stripes on the shoulders, a ribbed collar, cuffs, and hem for a snug fit, and the 'Brocade' logo embroidered on the sleeve. This jacket comes in various color options, making it perfect for elevating your streetwear look with a touch of luxury and modern flair."
+    },
+    {
+        id:5,
+        name:"SATIN GREEN BOMBER JACKET",
+        front_img :satingreenfront ,
+        back_img : satingreenback,
+        price: 5000.00,
+        desc: "Introducing the Satin Bomber Jacket—a bold and stylish addition to your wardrobe. Crafted with a sleek satin finish, it features striking double white stripes on the shoulders, a ribbed collar, cuffs, and hem for a snug fit, and the 'Brocade' logo embroidered on the sleeve. This jacket comes in various color options, making it perfect for elevating your streetwear look with a touch of luxury and modern flair."
+    },
+    {
+        id:6,
+        name:"MONOGRAM FUR HOODIE",
+        front_img : brocadenewhoodiefront,
+        back_img : brocadenewhoodieback,
+        price: 3500.00,
+        desc: "Introducing the Satin Bomber Jacket—a bold and stylish addition to your wardrobe. Crafted with a sleek satin finish, it features striking double white stripes on the shoulders, a ribbed collar, cuffs, and hem for a snug fit, and the 'Brocade' logo embroidered on the sleeve. This jacket comes in various color options, making it perfect for elevating your streetwear look with a touch of luxury and modern flair."
+    },
+
+]
+   
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { CiMenuBurger } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
+import logo from '../assets/brocade-logo.avif';
+import { VscAccount } from "react-icons/vsc";
+import { GiShoppingBag } from "react-icons/gi";
+import { useNavigate } from 'react-router-dom';
+
 
 export const Navbar = () => {
+
+  const navbar = useRef(null);
+  const navigate = useNavigate();
+  const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY);
+
+  useEffect(()=>{
+    const handleScroll = () => {
+        const currentScrollPos = window.scrollY;
+        if(prevScrollPos > currentScrollPos){
+              navbar.current.style.top = '0px'
+            }
+        else{
+             navbar.current.style.top = '-130px'
+            }
+            setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll',handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll); // Cleanup on unmount
+    };
+  },[prevScrollPos])
+
   return (
-    <div>Navbar</div>
+    <nav ref={navbar} className='flex justify-between items-center py-6 px-10 bg-[#E11F2C] sticky z-10 transition-all duration-500 scroll-smooth '>
+     <div className='flex items-center gap-5'>
+        <CiMenuBurger className='text-2xl text-white cursor-pointer hover:scale-110 transition-all duration-100 ease-in-out' />
+        <CiSearch className='text-2xl text-white cursor-pointer hover:scale-110 transition-all duration-100 ease-in-out' />
+      </div>
+      <div className='w-[5rem] h-auto' >
+        <img onClick={()=>navigate('/')} className='w-full h-full cursor-pointer swing-animation' src={logo} alt="" />
+      </div>
+      <div className='flex items-center gap-5'>
+        <VscAccount className='text-2xl text-white cursor-pointer hover:scale-110 transition-all duration-100 ease-in-out' />
+        <GiShoppingBag onClick={()=>navigate('/cart')}  className='text-2xl text-white cursor-pointer hover:scale-110 transition-all duration-100 ease-in-out'/>
+      </div>
+    </nav>
   )
 }
